3f23fa70a3c3412d24eb3884a83d30d6f4603a7e
MiguelFM26 <killerexpert26@gmail.com>
2026-07-31 12:21:39 -0500

    Enable CORS for Flutter Web

--- config/cors.php
+++ config/cors.php
@@ -14,14 +14,20 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['*'],
-'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'max_age' => 0,
-'supports_credentials' => false,
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'], // Permite peticiones desde Chrome local y desde Netlify
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
 
 ];
